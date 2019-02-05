fef258bd93566c9270fe59caedea0f310c555f70
padmanabhan21 <padmanabhan210193@gmail.com>
2019-02-05 10:26:44 +0530

    done

--- src/pages/dishes/dishes.ts
+++ src/pages/dishes/dishes.ts
@@ -17,6 +17,8 @@ import {ItempagePage} from '../itempage/itempage';
 export class DishesPage {
 
   public dishes_starter:any;
+  public dishes_main:any;
+  public dishes_special:any;
     
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -31,9 +33,22 @@ export class DishesPage {
     item_amount:230,item_helthtips:"take one soup daily"},
     {item_id:3,item_img:'../assets/imgs/gobi-65.jpg',item_name:"Panner 65",item_description:"hello darling",
     item_amount:230,item_helthtips:"take one soup daily"},
-    {item_id:4,item_img:'../assets/imgs/creditcards.jpg',item_name:"Gobi 65",item_description:"hello darling",
+    {item_id:4,item_img:'../assets/imgs/starter-4.jpg',item_name:"Gobi 65",item_description:"hello darling",
     item_amount:230,item_helthtips:"take one soup daily"},
-    {item_id:5,item_img:'../assets/imgs/creditcards.jpg',item_name:"Crispy veg",item_description:"hello darling",
+    {item_id:5,item_img:'../assets/imgs/starter-5.jpg',item_name:"Crispy veg",item_description:"hello darling",
+    item_amount:230,item_helthtips:"take one soup daily"}
+    ];
+
+    this.dishes_main = [
+      {item_id:1,item_img:'../assets/imgs/main-1.jpg',item_name:"Chicken Biriyani",item_description:"hello darling",
+    item_amount:230,item_helthtips:"take one soup daily"},
+    {item_id:2,item_img:'../assets/imgs/main-2.jpg',item_name:"Mutton Biriyani",item_description:"hello darling",
+    item_amount:230,item_helthtips:"take one soup daily"},
+    {item_id:3,item_img:'../assets/imgs/main-3.jpg',item_name:"Fish Dum Biriyani",item_description:"hello darling",
+    item_amount:230,item_helthtips:"take one soup daily"},
+    {item_id:4,item_img:'../assets/imgs/main-4.jpg',item_name:"Vegetable Biriyani",item_description:"hello darling",
+    item_amount:230,item_helthtips:"take one soup daily"},
+    {item_id:5,item_img:'../assets/imgs/main-5.jpg',item_name:"Ghee Rice",item_description:"hello darling",
     item_amount:230,item_helthtips:"take one soup daily"}
     ]
   }
